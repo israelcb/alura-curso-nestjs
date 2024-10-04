423832fff8b14f3a31e1cd8c9223e2f0317311c1
Israel Batista <israel.batista031@gmail.com>
2024-10-04 11:57:51 -0300

    Módulo 3 - validação síncrona

--- src/ussuario/usuario.controller.ts
+++ src/ussuario/usuario.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
+import { CriaUsuarioDTO } from "./dto/CriaUsuario.dto";
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -12,7 +13,7 @@ export class UsuarioController {
     }
 
     @Post()
-    async criaUsuario(@Body() dadosDoUsuario) {
+    async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
         this.usuarioRepository.salvar(dadosDoUsuario)
         return dadosDoUsuario
     }
